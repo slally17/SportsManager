90a5cf334d107ff6e69295886438ac15cfa5e46b
Sam <slally17@vt.edu>
2021-11-26 19:47:04 -0500

    Core data searching added.

--- SportsManager/Core Data Model/BasketballPlayer.swift
+++ SportsManager/Core Data Model/BasketballPlayer.swift
@@ -19,3 +19,76 @@ public class BasketballPlayer: NSManagedObject, Identifiable {
     @NSManaged public var team: Team?
     @NSManaged public var photo: Photo?
 }
+
+extension BasketballPlayer {
+
+    static func allBasketballPlayersFetchRequest() -> NSFetchRequest<BasketballPlayer> {
+        /*
+         Create a fetchRequest to fetch BasketballPlayer entities from the dataBasket.
+         Since the fetchRequest's 'predicate' property is not set to filter,
+         all of the BasketballPlayer entities will be fetched.
+         */
+        let fetchRequest = NSFetchRequest<BasketballPlayer>(entityName: "BasketballPlayer")
+        /*
+         List the fetched basketball players in alphabetical order with respect to name
+         */
+        fetchRequest.sortDescriptors = [
+            // Primary sort key: name
+            NSSortDescriptor(key: "name", ascending: false)
+        ]
+        
+        return fetchRequest
+    }
+    
+    /*
+     ❎ CoreData @FetchRequest in PlayersList.swift invokes this class method
+        to fetch filtered BasketballPlayer entities from the dataBasket for the given search query.
+        The 'static' keyword designates the func as a class method invoked by using the
+        class name as BasketballPlayer.filteredBasketballPlayersFetchRequest() in any .swift file in your project.
+     */
+    static func filteredBasketballPlayersFetchRequest(searchCategory: String, searchQuery: String) -> NSFetchRequest<BasketballPlayer> {
+        /*
+         Create a fetchRequest to fetch BasketballPlayer entities from the dataBasket.
+         Since the fetchRequest's 'predicate' property is set to a NSPredicate condition,
+         only those BasketballPlayer entities satisfying the condition will be fetched.
+         */
+        let fetchRequest = NSFetchRequest<BasketballPlayer>(entityName: "BasketballPlayer")
+        
+        /*
+         List the fetched Basketball players in alphabetical order with respect to name
+         */
+        fetchRequest.sortDescriptors = [
+            // Primary sort key: name
+            NSSortDescriptor(key: "name", ascending: false),
+        ]
+        
+        /*
+         Created NSPredicate object represents a condition or a compound condition with
+         AND/OR logical operators, which is used to filter fetching from the dataBasket.
+         */
+        switch searchCategory {
+        case "Player Name":
+            fetchRequest.predicate = NSPredicate(format: "name CONTAINS[c] %@", searchQuery)
+        case "Trip Rating":
+            fetchRequest.predicate = NSPredicate(format: "rating CONTAINS[c] %@", searchQuery)
+        case "Trip Title":
+            fetchRequest.predicate = NSPredicate(format: "title CONTAINS[c] %@", searchQuery)
+        case "Trip Start Date":
+            fetchRequest.predicate = NSPredicate(format: "startDate CONTAINS[c] %@", searchQuery)
+        case "Trip End Date":
+            fetchRequest.predicate = NSPredicate(format: "endDate CONTAINS[c] %@", searchQuery)
+        case "Trip Notes":
+            fetchRequest.predicate = NSPredicate(format: "notes CONTAINS[c] %@", searchQuery)
+        case "Compound":
+            let components = searchQuery.components(separatedBy: "AND")
+            let startDateQuery = components[0].trimmingCharacters(in: .whitespacesAndNewlines)
+            let ratingQuery = components[1].trimmingCharacters(in: .whitespacesAndNewlines)
+
+            fetchRequest.predicate = NSPredicate(format: "startDate CONTAINS[c] %@ AND rating CONTAINS[c] %@", startDateQuery, ratingQuery)
+        default:
+            print("Search category is out of range")
+        }
+        
+        return fetchRequest
+    }
+}
--- SportsManager/Core Data Model/Team.swift
+++ SportsManager/Core Data Model/Team.swift
@@ -19,3 +19,76 @@ public class Team: NSManagedObject, Identifiable {
     @NSManaged public var footballPlayers: NSSet?
     @NSManaged public var photo: Photo?
 }
+
+extension Team {
+
+    static func allTeamsFetchRequest() -> NSFetchRequest<Team> {
+        /*
+         Create a fetchRequest to fetch Team entities from the database.
+         Since the fetchRequest's 'predicate' property is not set to filter,
+         all of the Team entities will be fetched.
+         */
+        let fetchRequest = NSFetchRequest<Team>(entityName: "Team")
+        /*
+         List the fetched teams in alphabetical order with respect to name
+         */
+        fetchRequest.sortDescriptors = [
+            // Primary sort key: name
+            NSSortDescriptor(key: "name", ascending: false)
+        ]
+        
+        return fetchRequest
+    }
+    
+    /*
+     ❎ CoreData @FetchRequest in TeamsList.swift invokes this class method
+        to fetch filtered Team entities from the database for the given search query.
+        The 'static' keyword designates the func as a class method invoked by using the
+        class name as Team.filteredTeamsFetchRequest() in any .swift file in your project.
+     */
+    static func filteredTeamsFetchRequest(searchCategory: String, searchQuery: String) -> NSFetchRequest<Team> {
+        /*
+         Create a fetchRequest to fetch Team entities from the database.
+         Since the fetchRequest's 'predicate' property is set to a NSPredicate condition,
+         only those Team entities satisfying the condition will be fetched.
+         */
+        let fetchRequest = NSFetchRequest<Team>(entityName: "Team")
+        
+        /*
+         List the fetched teams in alphabetical order with respect to name
+         */
+        fetchRequest.sortDescriptors = [
+            // Primary sort key: name
+            NSSortDescriptor(key: "name", ascending: false),
+        ]
+        
+        /*
+         Created NSPredicate object represents a condition or a compound condition with
+         AND/OR logical operators, which is used to filter fetching from the database.
+         */
+        switch searchCategory {
+        case "Team Name":
+            fetchRequest.predicate = NSPredicate(format: "name CONTAINS[c] %@", searchQuery)
+        case "Trip Rating":
+            fetchRequest.predicate = NSPredicate(format: "rating CONTAINS[c] %@", searchQuery)
+        case "Trip Title":
+            fetchRequest.predicate = NSPredicate(format: "title CONTAINS[c] %@", searchQuery)
+        case "Trip Start Date":
+            fetchRequest.predicate = NSPredicate(format: "startDate CONTAINS[c] %@", searchQuery)
+        case "Trip End Date":
+            fetchRequest.predicate = NSPredicate(format: "endDate CONTAINS[c] %@", searchQuery)
+        case "Trip Notes":
+            fetchRequest.predicate = NSPredicate(format: "notes CONTAINS[c] %@", searchQuery)
+        case "Compound":
+            let components = searchQuery.components(separatedBy: "AND")
+            let startDateQuery = components[0].trimmingCharacters(in: .whitespacesAndNewlines)
+            let ratingQuery = components[1].trimmingCharacters(in: .whitespacesAndNewlines)
+
+            fetchRequest.predicate = NSPredicate(format: "startDate CONTAINS[c] %@ AND rating CONTAINS[c] %@", startDateQuery, ratingQuery)
+        default:
+            print("Search category is out of range")
+        }
+        
+        return fetchRequest
+    }
+}
