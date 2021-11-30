d8a9eab4a67abe6b8934b0c256c05c171b325219
KevinKrupa13 <kevinkrupa13@vt.edu>
2021-11-30 02:17:24 -0500

    Working on lists

---
+++ SportsManager/Tab Bar Views/Teams/TeamRoster.swift
@@ -0,0 +1,21 @@
+//
+//  TeamRoster.swift
+//  TeamRoster
+//
+//  Created by Kevin Krupa on 11/30/21.
+//  Copyright © 2021 CS3714 Team 7. All rights reserved.
+//
+
+import SwiftUI
+
+struct TeamRoster: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeamRoster_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamRoster()
+    }
+}
