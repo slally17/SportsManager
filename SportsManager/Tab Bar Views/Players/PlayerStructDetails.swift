0e3ff956fa543cee283d50e6f56e1fc4310da546
KevinKrupa13 <kevinkrupa13@vt.edu>
2021-12-07 14:46:20 -0500

    Player Roster

---
+++ SportsManager/Tab Bar Views/Players/PlayerStructDetails.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerStructDetails.swift
+//  PlayerStructDetails
+//
+//  Created by Kevin Krupa on 12/7/21.
+//  Copyright © 2021 CS3714 Team 7. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerStructDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerStructDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerStructDetails()
+    }
+}
---
+++ SportsManager/Tab Bar Views/Players/PlayerStructItem.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerStructItem.swift
+//  PlayerStructItem
+//
+//  Created by Kevin Krupa on 12/7/21.
+//  Copyright © 2021 CS3714 Team 7. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerStructItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerStructItem_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerStructItem()
+    }
+}
