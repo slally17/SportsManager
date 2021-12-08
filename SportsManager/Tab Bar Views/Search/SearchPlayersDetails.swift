4b15a2b86b4241fb6b411f970cef328af9e97c1a
Sam <slally17@vt.edu>
2021-12-08 01:30:56 -0500

    Player/team add from api added.

--- SportsManager/Tab Bar Views/Search/SearchPlayersDetails.swift
+++ SportsManager/Tab Bar Views/Search/SearchPlayersDetails.swift
@@ -10,6 +10,14 @@ import SwiftUI
 
 struct SearchPlayersDetails: View {
     
+    // Enable this View to be dismissed to go back when the Save button is tapped
+    @Environment(\.presentationMode) var presentationMode
+    
+    // ❎ CoreData managedObjectContext reference
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @State private var showChangesSavedAlert = false
+    
     let player: PlayerStruct
     
     var body: some View {
@@ -52,6 +60,67 @@ struct SearchPlayersDetails: View {
             }
         } // End of Form
         .navigationBarTitle(Text("\(player.name) Details"), displayMode: .inline)
+        .alert(isPresented: $showChangesSavedAlert, content: { changesSavedAlert })
         .font(.system(size: 14))
+        .navigationBarItems(trailing:
+            Button(action: {
+                addNewPlayer()
+                showChangesSavedAlert = true
+            }) {
+                Text("Add")
+            })
     } // End of body
+    
+    /*
+     -------------------
+     MARK: Alert Message
+     -------------------
+     */
+    var changesSavedAlert: Alert {
+        return Alert(title: Text("Player Added!"),
+          message: Text("Your player have been successfully saved to the database."),
+          dismissButton: .default(Text("OK")) {
+              presentationMode.wrappedValue.dismiss()
+            })
+    }
+    
+    /*
+     --------------------
+     MARK: Add New Player
+     --------------------
+     */
+    func addNewPlayer() {
+        
+        // ❎ Create a new Player entity in CoreData managedObjectContext
+        let newPlayer = Player(context: managedObjectContext)
+        
+        // ❎ Dress up the new Player entity
+        newPlayer.name = player.name
+        newPlayer.sport = player.sport
+        newPlayer.position = player.position
+        newPlayer.height = player.height
+        newPlayer.weight = player.weight
+        newPlayer.about = player.about
+        newPlayer.favorite = true
+        
+        // ❎ Create an instance of the Photo Entity in CoreData managedObjectContext
+        let photoEntity = Photo(context: managedObjectContext)
+        // Obtain the  photo image from Assets.xcassets as UIImage
+        let photoUIImage = getUIImageFromUrl(url: player.photoUrl, defaultFilename: "ImageUnavailable")
+        // Convert photoUIImage to data of type Data (Binary Data) in JPEG format with 100% quality
+        let photoData = photoUIImage.jpegData(compressionQuality: 1.0)
+        // Assign photoData to Core Data entity attribute of type Data (Binary Data)
+        photoEntity.photoData = photoData!
+        
+        // ❎ Establish One-to-One relationship between Player and Photo
+        newPlayer.photo = photoEntity      // A player can have only one photo
+        photoEntity.player = newPlayer      // A photo can belong to only one player
+        
+        // ❎ CoreData Save operation
+        do {
+            try managedObjectContext.save()
+        } catch {
+            return
+        }
+    }
 }
--- SportsManager/Tab Bar Views/Search/SearchTeamsDetails.swift
+++ SportsManager/Tab Bar Views/Search/SearchTeamsDetails.swift
@@ -10,6 +10,14 @@ import SwiftUI
 
 struct SearchTeamsDetails: View {
     
+    // Enable this View to be dismissed to go back when the Save button is tapped
+    @Environment(\.presentationMode) var presentationMode
+    
+    // ❎ CoreData managedObjectContext reference
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @State private var showChangesSavedAlert = false
+    
     let team: TeamStruct
     
     var body: some View {
@@ -34,6 +42,63 @@ struct SearchTeamsDetails: View {
             }
         } // End of Form
         .navigationBarTitle(Text("\(team.name) Details"), displayMode: .inline)
+        .alert(isPresented: $showChangesSavedAlert, content: { changesSavedAlert })
         .font(.system(size: 14))
+        .navigationBarItems(trailing:
+            Button(action: {
+                addNewTeam()
+                showChangesSavedAlert = true
+            }) {
+                Text("Add")
+            })
     } // End of body
+    
+    /*
+     -------------------
+     MARK: Alert Message
+     -------------------
+     */
+    var changesSavedAlert: Alert {
+        return Alert(title: Text("Team Added!"),
+          message: Text("Your team have been successfully saved to the database."),
+          dismissButton: .default(Text("OK")) {
+              presentationMode.wrappedValue.dismiss()
+            })
+    }
+    
+    /*
+     ------------------
+     MARK: Add New Team
+     ------------------
+     */
+    func addNewTeam() {
+        // ❎ Create a new Team entity in CoreData managedObjectContext
+        let newTeam = Team(context: managedObjectContext)
+        
+        // ❎ Dress up the new Team entity
+        newTeam.name = team.name
+        newTeam.sport = team.sport
+        newTeam.league = team.league
+        newTeam.about = team.about
+        
+        // ❎ Create an instance of the Photo Entity in CoreData managedObjectContext
+        let photoEntity = Photo(context: managedObjectContext)
+        // Obtain the  photo image from Assets.xcassets as UIImage
+        let photoUIImage = getUIImageFromUrl(url: team.photoUrl, defaultFilename: "ImageUnavailable")
+        // Convert photoUIImage to data of type Data (Binary Data) in JPEG format with 100% quality
+        let photoData = photoUIImage.jpegData(compressionQuality: 1.0)
+        // Assign photoData to Core Data entity attribute of type Data (Binary Data)
+        photoEntity.photoData = photoData!
+        
+        // ❎ Establish One-to-One relationship between Team and Photo
+        newTeam.photo = photoEntity      // A team can have only one photo
+        photoEntity.team = newTeam      // A photo can belong to only one team
+        
+        // ❎ CoreData Save operation
+        do {
+            try managedObjectContext.save()
+        } catch {
+            return
+        }
+    }
 }
